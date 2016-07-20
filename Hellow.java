e98dcb27e8c69efc099e9fd7b89bded9cb454810
ChoeYeonUg <vincentyeon@gmail.com>
2016-07-20 15:16:28 +0900

    Hellow.java modification on GitHub

--- Hellow.java
+++ Hellow.java
@@ -1,7 +1,10 @@
+//for command line git tutorial
+//Github modification 
+
 public class Hellow{
 
 	public static void main(String[] args){
-
+		System.out.println("Hell")
 		System.out.println("kikikikikiki");
 
 	}
